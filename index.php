eca84c3cffcf4497ab68350405dd1761abdcd5c3
M4tte78 <baillymatteo@icloud.com>
2024-02-16 14:26:56 +0100

    fix search-bar

--- index.php
+++ index.php
@@ -21,7 +21,25 @@
             <input type="search" class="board-search-input" aria-label="Board Search">
             <i class="fas fa-search search-icon" aria-hidden="true"></i>
         </div>
-        
+       <script>
+        document.addEventListener('DOMContentLoaded', function() {
+    var searchInput = document.querySelector('.board-search-input');
+
+    searchInput.addEventListener('input', function() {
+        var searchTerm = searchInput.value.toLowerCase();
+
+        var lists = document.querySelectorAll('.list');
+        for (var i = 0; i < lists.length; i++) {
+            var listTitle = lists[i].querySelector('.list-title').innerText.toLowerCase();
+            if (listTitle.includes(searchTerm)) {
+                lists[i].style.display = 'block';
+            } else {
+                lists[i].style.display = 'none';
+            }
+        }
+    });
+});
+</script>
 
     </div>
 
--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 var searchInput = document.querySelector('.board-search-input');
-var searchIcon = document.querySelector('.search-icon');
 
-searchIcon.addEventListener('click', function() {
-    var searchTerm = searchInput.value.toLowerCase();
+        searchInput.addEventListener('input', function() {
+            var searchTerm = searchInput.value.toLowerCase();
 
-    var lists = document.querySelectorAll('.list');
-    for (var i = 0; i < lists.length; i++) {
-        var listTitle = lists[i].querySelector('.list-title').innerText.toLowerCase();
-        if (listTitle.includes(searchTerm)) {
-            lists[i].style.display = 'block';
-        } else {
-            lists[i].style.display = 'none';
-        }
-    }
-});
+            var lists = document.querySelectorAll('.list');
+            for (var i = 0; i < lists.length; i++) {
+                var listTitle = lists[i].querySelector('.list-title').innerText.toLowerCase();
+                if (listTitle.includes(searchTerm)) {
+                    lists[i].style.display = 'block';
+                } else {
+                    lists[i].style.display = 'none';
+                }
+            }
+        });
